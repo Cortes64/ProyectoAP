7c2b8c52e91d82171087e09b6bf6366a22d634a5
EACortes8792 <esteban01cortes@gmail.com>
2025-04-03 18:29:36 -0600

    Se agregan cosas 2.0

--- app/src/main/java/com/altrik/proyectoap/utilities/Oferta.kt
+++ app/src/main/java/com/altrik/proyectoap/utilities/Oferta.kt
@@ -6,4 +6,10 @@ data class Oferta (
     val departamento: String,
     val descripcion: String,
     val profesor: String,
+    val fechaInicio: String,
+    val fechaFin: String,
+    val objetivos: String,
+    val cantidadVacantes: String,
+    val duracion: String,
+    val requisitos: String,
 )
